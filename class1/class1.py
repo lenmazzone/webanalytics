d1c836e7ae0cf8ade545aa0a21cd3cb696221e59
len@minsilo.com <len@minsilo.com>
2022-08-31 17:10:53 -0400

    mid  class 1

---
+++ class1/class1.py
@@ -0,0 +1,14 @@
+# this is a comment
+print(4 + 5 - (24 * 9) + 2 ** 20)
+print(3 / 2)
+x = 3 / 2
+print(x)
+
+x = abs(-34.3)
+print(x)
+print(round(4.6354645634563456345634563434634534, 5) + 5)
+print(str(x))
+
+x = 15
+print(x)
+
