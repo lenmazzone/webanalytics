940c1424e7d7ca3fdec9e59137dbeeaa5fc1872c
len@minsilo.com <len@minsilo.com>
2022-08-31 16:45:26 -0400

    beginning of class 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # hello world
+print("Hello world")
+print("This is a python program!")
+
+thisString = "I am a string stored in a variable"
+
+print(thisString)
+
+
+def thisfunction(count):
+    for char in thisString:
+        print(char)
+    while count > 0:
+        print("this is a while loop on pass:", count)
+        count -= 1
+
+
+thisfunction(10)
