8972542cab868b555626b0f22d1bf5ab168e295b
len@minsilo.com <len@minsilo.com>
2022-09-21 17:08:57 -0400

    data exercise

--- class3/class3.py
+++ class3/class3.py
@@ -21,7 +21,13 @@ list1 = [1, 3, 5]
 list2 = [2, 4, 6]
 
 for x in list1:
-    for y in list2:
-        print(f"{x * y}")
+    if x > 3:
+        for y in list2:
+            print(f"{x * y}")
 
     print(f"\n")
+
+better_list = ["string", "more string", 0, 1, 2, 3]
+
+for i in range(len(better_list)):
+    print(f"{better_list[i]}")
---
+++ class3/inclassexercise.py
@@ -0,0 +1,10 @@
+import os
+
+print(os.getcwd())
+os.chdir("../")
+print(os.getcwd())
+os.chdir("class3")
+
+with open("simple_file.txt", mode="r", encoding="utf-8") as f:
+    my_file = f.read()
+print(my_file)
