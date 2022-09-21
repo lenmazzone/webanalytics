6f680419a57b696ee0e33bce1fea5ceb985de901
len@minsilo.com <len@minsilo.com>
2022-09-21 17:38:50 -0400

    class over

--- class3/data_exercise/data.py
+++ class3/data_exercise/data.py
@@ -1,1 +1,25 @@
+with open('amazon_reviews.txt', mode='r', encoding="utf-8") as f:
+    reviews = f.read().split("\n")
+print(reviews[500])
 
+for i in range(len(reviews)):
+    reviews[i] = reviews[i].split("\t")
+
+users = set()
+
+for i in range(len(reviews)):
+    users.add(reviews[i][0])
+    # print(reviews[i][0])
+
+print(users, "\n", len(users), len({k[0] for k in reviews}))
+
+print(reviews[500])
+
+raters = dict()
+
+for r in reviews:
+    if r[1] not in raters:
+        raters[r[1]] = 1
+    else:
+        raters[r[1]] += 1
+print(raters)
