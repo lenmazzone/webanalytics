cce67297963c6c600e784221294e7b91cb95166c
Leonard Mazzone <len@minsilo.com>
2022-09-26 12:25:41 -0400

    completed

--- real_hw1/homework1.py
+++ real_hw1/homework1.py
@@ -25,11 +25,11 @@ print(ratings)
 # Question 4
 distribution = dict()
 
-for r in statements:
-    if r[0] not in distribution:
-        distribution[r[0]] = 1
+for rating in statements:
+    if rating[0] not in distribution:
+        distribution[rating[0]] = 1
     else:
-        distribution[r[0]] += 1
+        distribution[rating[0]] += 1
 
 print(distribution)
 
@@ -47,17 +47,43 @@ for item in new_statements:
 
 print(new_statements[0])
 
+
 # Question 6
-for item in new_statements:
+def count_truths(name):
     lie_counter = 0
     truth_counter = 0
+    for s in new_statements:
+        if s[2] == name:
+            # print(f"in a {name} file")
+            if s[0] == 0:
+                # print("One lie!")
+                lie_counter += 1
+            else:
+                # print("One Truth!")
+                truth_counter += 1
+    print(f"Total {name} lies: {lie_counter}\nTotal {name} truths: {truth_counter}\n")
 
-    if item[2] == "donald=trump":
-        if item[0] == 0:
-            print("One lie!")
-            lie_counter += 1
-        else:
-            print("One Truth!")
-            truth_counter += 1
 
-print(f"Total lies: {lie_counter}\nTotal truths: {truth_counter}")
+count_truths("donald-trump")
+
+# Question 7
+# See function defined for Question 6
+count_truths("hillary-clinton")
+
+count_truths("barack-obama")
+
+# Question 8
+subjects = dict()
+for s in new_statements:
+    if s[2] == "barack-obama":
+        for item in s[5]:
+            if item not in subjects:
+                subjects[item] = 1
+            else:
+                subjects[item] += 1
+
+# print(subjects)
+sorted_tuples = sorted(subjects.items(), key=lambda item: item[1], reverse=True)
+# In theory the answer could be: print(sorted_tuples[0:10]) but I think the below is a little cleaner.
+top_ten_subjects = {k: v for k, v in sorted_tuples[0:10]}
+print(f"The top ten subjects and # of mentions are: {top_ten_subjects}")
